260e3a1f8815402834139ac4b585dfedaec4d116
Harshal <harshalubale4@gmail.com>
2022-03-15 17:51:33 +0530

    Implemented the Delete and GetById end points

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -3,14 +3,21 @@ const JWT_SEC = 'thisisajwtsecret';
 const Admin = require('../models/admin');
 
 module.exports.isLoggedIn = async (req, res, next) => {
-    const token = req.header('auth-token');
-    if (!token) {
-        res.status(401).send({ error: "Please Authenticate First" });
+    try {
+
+        const token = req.header('auth-token');
+        if (!token) {
+            res.status(401).send({ error: "Please Authenticate First" });
+        }
+        const tokenData = jwt.verify(JSON.parse(token), JWT_SEC);
+        const admin = await Admin.findOne({ _id: tokenData.admin.id });
+        if (!admin) {
+            res.status(401).send({ error: "Please Authenticate First" });
+        }
+    } catch (e) {
+        console.log(e);
+        res.json({ error: 'An Error has Occured', message: e.message });
     }
-    const tokenData = jwt.verify(token, JWT_SEC);
-    const admin = await Admin.findOne({ _id: newToken.admin.id });
-    if (!admin) {
-        res.status(401).send({ error: "Please Authenticate First" });
-    }
+
     next();
 }
--- server/routes/content.js
+++ server/routes/content.js
@@ -1,8 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const Content = require('../models/content');
+const { isLoggedIn } = require('../middleware/middleware');
 
-router.get('/', (req, res) => {
-    console.log()
+
+router.post('/', [
+    body('title', 'Enter a Title of Min Length 5').isLength({ min: 5 }).exists(),
+    body('quote', "Please Enter a Quote of Min Length 10").isLength(10).exists(),
+    body('about', 'Please Write something in About Section of Min Length 20').isLength({ min: 20 }).exists()
+], isLoggedIn, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const content = await Content.create({
+            title: req.body.title,
+            quote: req.body.quote,
+            about: req.body.about
+        })
+        const response = await content.save();
+        console.log(response);
+        res.json({ response });
+
+    } catch (e) {
+        console.log(e);
+        res.json({ error: 'An Error has Occured', message: e.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const allContent = await Content.find({});
+        console.log(allContent);
+        res.json(allContent);
+    } catch (e) {
+        console.log(e);
+        res.json({ error: 'An Error has Occured', message: e.message });
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const specificContent = await Content.findOne({ _id: id });
+        res.json(specificContent);
+    } catch (e) {
+        console.log(e);
+        res.json({ error: 'An Error has Occured', message: e.message });
+    }
+})
+
+router.delete('/:id', isLoggedIn, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await Content.findOneAndDelete({ _id: id });
+        res.json(response);
+    } catch (e) {
+        console.log(e);
+        res.json({ error: 'An Error has Occured', message: e.message });
+    }
 })
 
 module.exports = router;
