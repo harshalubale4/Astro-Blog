f6d9898783f79f7c6de9ed2e782357dbac4eb43a
Harshal <harshalubale4@gmail.com>
2022-03-24 02:37:35 +0530

    Implemented Dark Mode for Pagination

--- client/src/Components/Pagination/CustomPagination.js
+++ client/src/Components/Pagination/CustomPagination.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Pagination from '@mui/material/Pagination';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 const CustomPagination = ({ setPage, numberOfPages }) => {
     const handlePageChange = (page) => {
         setPage(page);
         window.scroll(0, 0);
     }
+
     return (
         <div>
-            <Pagination count={numberOfPages} onChange={(e) => handlePageChange(e.target.textContent)} size="large" hidePrevButton hideNextButton />
+            <ThemeProvider theme={darkTheme} >
+                <Pagination
+                    count={numberOfPages}
+                    onChange={(e) => handlePageChange(e.target.textContent)}
+                    size="large"
+                    hidePrevButton
+                    hideNextButton />
+            </ThemeProvider>
         </div>
     )
 }
