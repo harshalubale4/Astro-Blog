a65f2cb1fef5b423440c10250d3535ec483d4422
Harshal <harshalubale4@gmail.com>
2022-03-24 21:02:45 +0530

    Ready to Deploy

---
+++ middleware/middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const JWT_SEC = 'thisisajwtsecret';
+const Admin = require('../models/admin');
+
+module.exports.isLoggedIn = async (req, res, next) => {
+    try {
+
+        const token = req.header('auth-token');
+        if (!token) {
+            res.status(401).send({ error: "Please Authenticate First" });
+        }
+        const tokenData = jwt.verify(JSON.parse(token), JWT_SEC);
+        const admin = await Admin.findOne({ _id: tokenData.admin.id });
+        if (!admin) {
+            res.status(401).send({ error: "Please Authenticate First" });
+        }
+    } catch (e) {
+        console.log(e);
+        res.json({ error: 'An Error has Occured checking if you were Authenticated or not...', message: e.message });
+    }
+
+    next();
+}
