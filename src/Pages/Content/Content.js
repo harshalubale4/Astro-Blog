4579afcce57bcc937a272fdc9ff8f9124f5147ca
Harshal <harshalubale4@gmail.com>
2022-03-25 11:18:00 +0530

    Resolved the Pagination glitch

--- src/Pages/Content/Content.js
+++ src/Pages/Content/Content.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Card from '../../Components/Card/Card';
 import CustomPagination from '../../Components/Pagination/CustomPagination';
 import PuffLoader from "react-spinners/PuffLoader";
-import './Content.css'
+import './Content.css';
 
 const Content = ({ showAlert }) => {
     const host = `${process.env.React_App_Server_Url}`;
@@ -38,34 +38,32 @@ const Content = ({ showAlert }) => {
         <>
             {
                 loading ? (
-                    <div className='text-center' style={{
+                    <div className='d-flex flex-row justify-content-center align-items-center' style={{
                         marginTop: "100px"
                     }}>
                         <PuffLoader color='#ffffff' size={150} margin={2} speedMultiplier={2} loading={loading} />
                     </div>
                 ) :
-                    <>
-                        <div className='d-flex flex-row justify-content-around align-items-center flex-wrap mx-auto content-container p-2'>
-                            {fetchedContent.map((elem) => {
-                                return (
-                                    <Card
-                                        key={elem._id}
-                                        title={elem.title}
-                                        quote={elem.quote}
-                                        about={elem.about}
-                                        id={elem._id}
-                                        imagesSrc={elem.images}
-                                    />
-                                )
-                            })}
-                        </div>
-                        <div className='d-flex flex-row justify-content-center mt-auto' id='paginationContainer'>
-                            <CustomPagination numberOfPages={numOfPages} setPage={setPage} />
-                        </div>
-                    </>
+                    <div className='d-flex flex-row justify-content-around align-items-center flex-wrap mx-auto content-container p-2'>
+                        {fetchedContent.map((elem) => {
+                            return (
+                                <Card
+                                    key={elem._id}
+                                    title={elem.title}
+                                    quote={elem.quote}
+                                    about={elem.about}
+                                    id={elem._id}
+                                    imagesSrc={elem.images}
+                                />
+                            )
+                        })}
+                    </div>
             }
+            <div className='d-flex flex-row justify-content-center mt-auto' id='paginationContainer'>
+                <CustomPagination numberOfPages={numOfPages} setPage={setPage} />
+            </div>
         </>
     )
 }
 
-export default Content
+export default Content;
