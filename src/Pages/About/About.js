f942c2efecabaec4df8941d81a69ce491060db0c
Harshal <harshalubale4@gmail.com>
2022-03-25 11:44:58 +0530

    Testing if client branch is connected to vercel or not

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,10 +4,10 @@ const About = () => {
     return (
         <>
             <div className='container text-center d-flex flex-column justify-content-center align-items-center mt-5'>
-                <h1>
+                <h1 className='display-5'>
                     Hey there, this is the place where Information about you can be displayed.
                     <br />
-                    I mean like your own Portfolio
+                    In short your own Portfolio
                 </h1>
             </div>
         </>
