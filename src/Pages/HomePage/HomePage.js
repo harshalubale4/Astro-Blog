8584cc08d0b3437ed6154c1ec21351e884c4800f
Harshal <harshalubale4@gmail.com>
2022-04-11 11:32:25 +0530

    Changes in Home Page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './HomePage.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "70%",
+    bgcolor: 'black',
+    border: '2px solid #000',
+    borderRadius: 8,
+    boxShadow: 24,
+    p: 4,
+};
 
 const HomePage = () => {
     const [titleOfImageOftheDay, setTitleOfImageOftheDay] = useState("");
     const [imageUrlOfTheDay, setImageUrlOfTheDay] = useState("");
-    const [descOfTheImageOfTheDay, setDescOfTheImageOfTheDay] = useState("")
+    const [descOfTheImageOfTheDay, setDescOfTheImageOfTheDay] = useState("");
+    const [copyRight, setCopyRight] = useState("");
+
     const getNasaImageOfTheDay = async () => {
         const respo = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.React_App_Nasa_Api_Key}`, {
             method: "GET",
@@ -15,20 +33,24 @@ const HomePage = () => {
         setImageUrlOfTheDay(json.hdurl);
         setTitleOfImageOftheDay(json.title);
         setDescOfTheImageOfTheDay(json.explanation);
+        setCopyRight(json.copyright);
     }
+
     useEffect(() => {
         getNasaImageOfTheDay();
     }, [])
 
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <>
-            <Link to='/content' className='circle d-flex flex-column justify-content-center align-items-center mx-auto my-5'>
-                <div className='explore'>
+            <Link to='/content' className='mx-auto my-5 text-decoration-none'>
+                <div id='explore' >
                     Explore
                 </div>
             </Link>
-
 
             <div className='container'>
                 <div
@@ -43,11 +65,29 @@ const HomePage = () => {
                     <div className='title mt-auto text-center'>
                         {titleOfImageOftheDay}
                     </div>
-
                 </div >
-                <div className='imageDesc'>
-                    {descOfTheImageOfTheDay}
-                </div>
+
+                <button onClick={handleOpen} className="btn mx-auto d-block my-3" id="readMoreButton">Read More</button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style} style={{ maxHeight: "65vh", overflow: 'auto' }}>
+                        <Typography id="modal-modal-title" variant="h4" component="h2" className='text-center'>
+                            {titleOfImageOftheDay}
+                        </Typography>
+                        <Typography
+                            className='text-center'
+                            id="modal-modal-description" sx={{ mt: 2 }}>
+                            {descOfTheImageOfTheDay}
+                        </Typography>
+                        <div className='text-muted my-3 text-center '>
+                            &#169; {copyRight}
+                        </div>
+                    </Box>
+                </Modal>
             </div>
         </>
     )
